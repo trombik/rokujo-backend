4dc50eb20cb4e8bd6e78d3406f4689c0cb202d0d
Tomoyuki Sakurai <y@trombik.org>
2026-02-21 18:21:17 +0700

    fix: allow the job to abort and enqueue jobs in batches

--- app/jobs/enqueue_token_analysis_job.rb
+++ app/jobs/enqueue_token_analysis_job.rb
@@ -3,10 +3,14 @@ class EnqueueTokenAnalysisJob < ApplicationJob
   limits_concurrency to: 1, key: -> { "enqueue_token_analysis" }, duration: 5.minutes
 
   def perform
-    Sentence.where.missing(:token_analyses)
-            .distinct
-            .find_each do |sentence|
-              AnalyzeTokensJob.perform_later(sentence.id)
-            end
+    Rails.cache.delete("stop_analysis_enqueue")
+    sentences = Sentence.where.missing(:token_analyses).distinct
+    sentences.in_batches(of: 200) do |relation|
+      break if Rails.cache.read("stop_analysis_enqueue")
+
+      jobs = relation.pluck(:id).map { |id| AnalyzeTokensJob.new(id) }
+      ActiveJob.perform_all_later(jobs)
+      sleep 0.1
+    end
   end
 end
