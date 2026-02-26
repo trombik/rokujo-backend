456fe2ce80e0d14713c803cec674c856f8278c13
Tomoyuki Sakurai <y@trombik.org>
2026-02-26 12:46:11 +0700

    chore: add rspec-html-matchers to gems

--- Gemfile
+++ Gemfile
@@ -104,6 +104,7 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
+  gem "rspec-html-matchers"
   gem "selenium-webdriver"
 end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,6 +79,9 @@ RSpec.configure do |config|
   config.include ViewComponent::TestHelpers, type: :component
   config.include ViewComponent::SystemTestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
+
+  # Use rspec-html-matchers for testing HTML tags
+  config.include RSpecHtmlMatchers
 end
 
 Shoulda::Matchers.configure do |config|
