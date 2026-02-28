a38d618e7aabfbad4a957b9f900a6d020df20263
Tomoyuki Sakurai <y@trombik.org>
2026-02-28 10:18:24 +0700

    fix: missing class documentation in Stats::SentenceAnalysisRatioComponent

--- app/components/stats/sentence_analysis_ratio_component.rb
+++ app/components/stats/sentence_analysis_ratio_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Displays analyzed sentences in percentage
 class Stats::SentenceAnalysisRatioComponent < Stats::SingleNumberComponent
   def frame_url
     @frame_url || stats_sentence_analysis_ratio_path
