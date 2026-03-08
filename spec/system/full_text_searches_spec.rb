9cf67e18e6b6e99064e6b652451196439c9dbe9c
Tomoyuki Sakurai <y@trombik.org>
2026-03-09 02:35:08 +0700

    refactor: full_text_searches_spec and add more tests

--- spec/system/full_text_searches_spec.rb
+++ spec/system/full_text_searches_spec.rb
@@ -1,16 +1,27 @@
 require "rails_helper"
 
 TARGET = "こんにちは".freeze
+TARGET_TAG = "ユニークなタグ".freeze
+TARGET_SITE_NAME = "サイト".freeze
+TARGET_DOMAIN = "example.org".freeze
 
 RSpec.describe "FullTextSearches", type: :system do
   include ERB::Util
 
+  let!(:another_sentence) do
+    create(:sentence, text: "さようなら",
+                      article: create(
+                        :article,
+                        site_name: TARGET_SITE_NAME,
+                        url: "http://#{TARGET_DOMAIN}/path2"
+                      ))
+  end
   let!(:sentence) do
     create(:sentence, text: TARGET,
                       article: create(
                         :article,
-                        site_name: "Site name",
-                        url: "http://example.org/path"
+                        site_name: TARGET_SITE_NAME,
+                        url: "http://#{TARGET_DOMAIN}/path1"
                       ))
   end
   let(:word) { "" }
@@ -18,8 +29,8 @@ RSpec.describe "FullTextSearches", type: :system do
   before do
     driven_by(:rack_test)
 
-    ac = create(:article_collection, key: "site_name", value: "Site name")
-    create(:collection_tag, name: "Example", article_collections: [ac])
+    ac = create(:article_collection, key: "site_name", value: TARGET_SITE_NAME)
+    create(:collection_tag, name: TARGET_TAG, article_collections: [ac])
 
     visit search_path
     within(format("form[action='%s']", search_path)) do
@@ -38,22 +49,34 @@ RSpec.describe "FullTextSearches", type: :system do
     TARGET,
     "#{TARGET}?",
     "#{TARGET} foobar", # when multipul keywords are given, the first one is used and the rest are ignored.
-    "site_name:\"Site name\"",
-    "#{TARGET} site_name:\"Site name\"",
-    "#{TARGET} url:example.org/path",
-    "#{TARGET} url:example.org",
-    "#{TARGET} site_name:\"Site name\" url:example.org",
-    "site_name:\"Site name\" #{TARGET} url:example.org",
-    "site_name:\"Site name\" url:example.org #{TARGET}",
-    "#{TARGET} tag:Example",
-    "#{TARGET} tag:Example url:example.net", # when a tag is given, other operators are ignored.
-    "#{TARGET} tag:Example site_name:foo"
+    "#{TARGET} site_name:\"#{TARGET_SITE_NAME}\"",
+    "#{TARGET} url:#{TARGET_DOMAIN}/path1",
+    "#{TARGET} url:#{TARGET_DOMAIN}",
+    "#{TARGET} site_name:\"#{TARGET_SITE_NAME}\" url:#{TARGET_DOMAIN}",
+    "site_name:\"#{TARGET_SITE_NAME}\" #{TARGET} url:#{TARGET_DOMAIN}",
+    "site_name:\"#{TARGET_SITE_NAME}\" url:#{TARGET_DOMAIN} #{TARGET}",
+    "#{TARGET} tag:#{TARGET_TAG}",
+    "#{TARGET} tag:#{TARGET_TAG} url:example.net", # when a tag is given, other operators are ignored.
+    "#{TARGET} tag:#{TARGET_TAG} site_name:foo"
   ].each do |keyword|
     context "when keyword is `#{keyword}`" do
       let(:word) { keyword }
 
-      it "shows the sentence" do
-        expect(page).to have_text(sentence.text)
+      it "shows the matched sentence only" do
+        expect(page).to have_text(sentence.text).and have_no_text(another_sentence.text)
+      end
+    end
+  end
+  [
+    "site_name:\"#{TARGET_SITE_NAME}\"",
+    "url:#{TARGET_DOMAIN}",
+    "tag:#{TARGET_TAG}"
+  ].each do |keyword|
+    context "when the operator (`#{keyword}`) matches the article" do
+      let(:word) { keyword }
+
+      it "shows both sentences" do
+        expect(page).to have_text(sentence.text).and have_text(another_sentence.text)
       end
     end
   end
@@ -66,18 +89,19 @@ RSpec.describe "FullTextSearches", type: :system do
     "siet_name:",
     "url:example.net",
     "#{TARGET} url:example.net",
+    "#{TARGET} url:http://example.org",
     "url:",
     "tag:Foo",
     "#{TARGET} tag:Foo", # when a tag is given, other operators are ignored.
-    "#{TARGET} tag:Foo site_name:\"Site name\"",
-    "#{TARGET} tag:Foo url:example.org",
+    "#{TARGET} tag:Foo site_name:\"#{TARGET_SITE_NAME}\"",
+    "#{TARGET} tag:Foo url:#{TARGET_DOMAIN}",
     "tag:"
   ].each do |keyword|
     context "when keyword is `#{keyword}`" do
       let(:word) { keyword }
 
       it "shows no results" do
-        expect(page).to have_no_text(TARGET)
+        expect(page).to have_no_text(TARGET).and have_no_text(another_sentence.text)
       end
     end
   end
