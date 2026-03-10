33def2f370a0ffd5530a85429c0a79da10a08b7a
Tomoyuki Sakurai <y@trombik.org>
2026-03-10 21:27:08 +0700

    chore: refine ArticleCollectionComponent with appropriate defaults

--- app/components/forms/article_collection_component.rb
+++ app/components/forms/article_collection_component.rb
@@ -4,8 +4,8 @@
 class Forms::ArticleCollectionComponent < ViewComponent::Base
   attr_reader :article_collection
 
-  def initialize(article_collection)
-    @article_collection = article_collection
+  def initialize(article_collection = nil)
+    @article_collection = article_collection || ArticleCollection.new(name: "", key: "site_name", value: "")
     super()
   end
 end
--- spec/components/previews/forms/article_collection_component_preview.rb
+++ spec/components/previews/forms/article_collection_component_preview.rb
@@ -2,7 +2,12 @@
 
 class Forms::ArticleCollectionComponentPreview < ViewComponent::Preview
   def default
-    article_collection = FactoryBot.build(:article_collection)
+    article_collection = nil
+    render Forms::ArticleCollectionComponent.new(article_collection)
+  end
+
+  def with_name
+    article_collection = ArticleCollection.new(key: "site_name", value: "foo")
     render Forms::ArticleCollectionComponent.new(article_collection)
   end
 end
