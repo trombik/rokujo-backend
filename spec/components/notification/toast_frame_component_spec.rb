83b98800ec364bc89c7a2849fc711ac5e6858afc
Tomoyuki Sakurai <y@trombik.org>
2026-03-11 02:52:48 +0700

    feat: introduce Notification::ToastComponent

---
+++ spec/components/notification/toast_frame_component_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Notification::ToastFrameComponent, type: :component do
+  let(:component) { described_class.new }
+
+  before do
+    render_inline component
+  end
+
+  it "has ID" do
+    expect(page).to have_css("div[id]")
+  end
+end
---
+++ spec/components/previews/notification/toast_frame_component_preview.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Notification::ToastFrameComponentPreview < ViewComponent::Preview
+  def default
+    render(Notification::ToastFrameComponent.new)
+  end
+end
