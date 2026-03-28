898a986f54b24fb719c52d8a7e58025cac26826d
Tomoyuki Sakurai <y@trombik.org>
2026-03-28 15:42:59 +0700

    feat: add a button to show all modifiers in Collocations::ModifierComponent
    to limit the vertical height of the component.

--- spec/components/previews/collocations/modifier_component_preview.rb
+++ spec/components/previews/collocations/modifier_component_preview.rb
@@ -3,7 +3,7 @@
 class Collocations::ModifierComponentPreview < ViewComponent::Preview
   def default
     type = "acl"
-    lemma = "データ"
+    lemma = "首相"
     results = NounPhraseExtractor.new(lemma: lemma, deps: [type]).call
     render Collocations::ModifierComponent.new(type, lemma, results)
   end
