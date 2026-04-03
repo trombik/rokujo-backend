564ea5182a3689e395100992745cbcdb633bb53c
Tomoyuki Sakurai <y@trombik.org>
2026-04-03 14:49:28 +0700

    fix: remove jobs obsoleted by ImportArticleJob

--- spec/jobs/import_article_job_spec.rb
+++ spec/jobs/import_article_job_spec.rb
@@ -12,24 +12,6 @@ RSpec.describe ImportArticleJob, type: :job do
       allow(Article).to receive(:import_from_hash!).and_return(article)
     end
 
-    it "enqueue AnalyzeTokensJob" do
-      expect do
-        described_class.perform_now({})
-      end.to have_enqueued_job(AnalyzeTokensJob)
-    end
-
-    context "when AnalyzeTokensJob failed to perform_later" do
-      before do
-        allow(AnalyzeTokensJob).to receive(:perform_later).and_raise(StandardError)
-      end
-
-      it "raises EnqueueError" do
-        expect do
-          described_class.perform_now({})
-        end.to raise_error ImportArticleJob::EnqueueError
-      end
-    end
-
     context "when the article is not unique" do
       before do
         allow(Article).to receive(:import_from_hash!).and_raise(ActiveRecord::RecordNotUnique)
